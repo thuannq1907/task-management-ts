2609e2b407176f0d118da791f5c71c221da48cb3
thuannq1907 <nguyenquangthuan190820@gmail.com>
2024-03-17 20:01:37 +0700

    Lấy ra danh sách công việc

--- index.ts
+++ index.ts
@@ -1,1 +1,30 @@
-console.log("OK");
+// const express = require("express");
+import express, { Express, Request, Response } from "express";
+
+// const dotenv = require("dotenv");
+// dotenv.config();
+import dotenv from "dotenv";
+dotenv.config();
+
+// const database = require("./config/database");
+// database.connect();
+import { connect as connectDatabase } from "./config/database";
+connectDatabase();
+
+// const Task = require("./models/task.model");
+import Task from "./models/task.model";
+
+const app: Express = express();
+const port: string | number = process.env.PORT || 3000;
+
+app.get("/tasks", async (req: Request, res: Response) => {
+  const tasks = await Task.find({
+    deleted: false
+  });
+
+  res.json(tasks);
+});
+
+app.listen(port, () => {
+  console.log(`App listening on port ${port}`);
+});
