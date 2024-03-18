0b8c01801d807cf33c223cb77bc2705d11ca2e81
thuannq1907 <nguyenquangthuan190820@gmail.com>
2024-03-18 17:21:29 +0700

    Xóa nhiều công việc

--- api/v1/controllers/task.controller.ts
+++ api/v1/controllers/task.controller.ts
@@ -96,6 +96,23 @@ export const changeMulti = async (req: Request, res: Response) => {
         });
         break;
 
+      case "delete":
+        await Task.updateMany(
+          {
+            _id: { $in: ids }
+          },
+          {
+            deleted: true,
+            deletedAt: new Date()
+          }
+        );
+
+        res.json({
+          code: 200,
+          message: "Xóa thành công!"
+        });
+        break;
+        
       default:
         res.json({
           code: 400,
