642ac4404a6c9dccd7979cd7ce6cdadc0d40b548
thuannq1907 <nguyenquangthuan190820@gmail.com>
2024-03-18 14:07:32 +0700

    Sắp xếp sản phẩm theo các tiêu chí khác nhau

--- api/v1/controllers/task.controller.ts
+++ api/v1/controllers/task.controller.ts
@@ -11,7 +11,15 @@ export const index = async (req: Request, res: Response) => {
     find["status"] = req.query.status;
   }
 
-  const tasks = await Task.find(find);
+  // Sort
+  const sort = {};
+
+  if(req.query.sortKey && req.query.sortValue) {
+    sort[`${req.query.sortKey}`] = req.query.sortValue;
+  }
+  // End Sort
+
+  const tasks = await Task.find(find).sort(sort);
 
   res.json(tasks);
 }
