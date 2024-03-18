c081e7965a372cfa29600a13a36be55cea76ca5c
thuannq1907 <nguyenquangthuan190820@gmail.com>
2024-03-18 17:00:29 +0700

    Thêm mới công việc

--- api/v1/controllers/task.controller.ts
+++ api/v1/controllers/task.controller.ts
@@ -110,3 +110,22 @@ export const changeMulti = async (req: Request, res: Response) => {
     });
   }
 };
+
+// [POST] /api/v1/tasks/create
+export const create = async (req: Request, res: Response) => {
+  try {
+    const task = new Task(req.body);
+    const data = await task.save();
+
+    res.json({
+      code: 200,
+      message: "Tạo thành công!",
+      data: data
+    });
+  } catch (error) {
+    res.json({
+      code: 400,
+      message: "Lỗi!"
+    });
+  }
+};
