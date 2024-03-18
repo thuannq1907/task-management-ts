970129f3d79c90fe1649094cec4740c3b914df2a
thuannq1907 <nguyenquangthuan190820@gmail.com>
2024-03-18 17:09:33 +0700

    Chỉnh sửa công việc

--- api/v1/controllers/task.controller.ts
+++ api/v1/controllers/task.controller.ts
@@ -129,3 +129,22 @@ export const create = async (req: Request, res: Response) => {
     });
   }
 };
+
+// [PATCH] /api/v1/tasks/edit/:id
+export const edit = async (req: Request, res: Response) => {
+  try {
+    const id: string = req.params.id;
+
+    await Task.updateOne({ _id: id }, req.body);
+
+    res.json({
+      code: 200,
+      message: "Cập nhật thành công!"
+    });
+  } catch (error) {
+    res.json({
+      code: 400,
+      message: "Lỗi!"
+    });
+  }
+};
